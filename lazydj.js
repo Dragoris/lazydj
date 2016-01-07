7aa41385723d3decfffe6881e35bcbc806a09e8e
Jason Pitts <jasonpitts66@gmail.com>
2016-01-07 14:50:18 -0800

    working play button!
    WOOOOOOO!

--- lazydj.js
+++ lazydj.js
@@ -13,8 +13,21 @@ imageObj.src = "images/Boose Boosington.jpg";
 
 // globals for tracks and playlists.
 var tracks, playlist = [];
+
 // play/pause button logic
-var current_player;
+var currentPlayer;
+var streamTrack = function(track){
+    console.log("track", track);
+    return SC.stream('/tracks/' + track.id).then(function(player){
+      if (currentPlayer) {
+        currentPlayer.pause();
+      }
+      currentPlayer = player;
+      player.play();
+    }).catch(function(){
+      console.log(arguments);
+    }); //end of return
+};
 
 // initializing soundcloud (SC object)
 SC.initialize({
@@ -27,14 +40,15 @@ $("#search").autocomplete({
         SC.get('/tracks', {q: request.term}).then(function (songs) {
             //clean out the display_results array. to be shown to the user by autocomplete.
             var display_results = [];
-            console.log(songs);
+            //console.log(songs);
             for (var i = 0; i < songs.length; i++) {
-                var song_obj = songs[i];
-                var id = i.toString() + " ";
-                if(song_obj.streamable) {
+                var songObj = songs[i];
+                var index = i.toString() + " ";
+                var uri = " " + songObj.uri;
+                if(songObj.streamable) {
                     display_results.push({
-                        label: song_obj.title,
-                        value: id + song_obj.id
+                        label: songObj.title,
+                        value: index + songObj.id + uri
                     });
                 }
                 else {
@@ -42,7 +56,7 @@ $("#search").autocomplete({
                 }
             } // end of for loop
             tracks = songs;
-            console.log("track", tracks);
+            //console.log("track", tracks);
             response(display_results);
         }).catch(function() {
             console.log("failed search", arguments);
@@ -52,25 +66,25 @@ $("#search").autocomplete({
     minLength: 3, //min input length needed to fire source anon func
     // select is run when user selects a link
     select: function (event, ui) {
-        
+        // ui variable is from the jquery autocomplete spec. We know it will have
+        // the value of the item selected in the drop down list.
+        // we are using the made the ui variable have 2 numbers.
+        // to extract the meaningful numbers we are spliting the sting.
         var split = ui.item.value.split(" ");
-        var streamplayer = streamsc(split[1]);
-        console.log(streamplayer);
-        streamplayer.play();
-        $(".playlist").append('<div class="queued-song"><li class="track-playlist"><img class="thumbnail" src='+tracks[split[0]].artwork_url+'>'+tracks[split[0]].title+'</li></div>');
-        SC.stream("/tracks/"+ split[1]).then(function(player){
-        if (current_player) {
-            current_player.pause();
-        }
-        current_player = player;
-        console.log(current_player,"current");
-        console.log(player);
-        player.play();
-        // inject html - will always do when a song is selected
-        $(".playlist").append('<div class="queued-song"><li class="track-playlist"><img class="thumbnail" src='+tracks[split[0]].artwork_url+'>'+tracks[split[0]].title+'</li></div>');
+        var songIndex = split[0];
+        var sondID = split[1];
+        var songUri = split[2];
+        SC.resolve(songUri).then(streamTrack);
+        $(".playlist").append('<div class="queued-song"><li class="track-playlist"><img class="thumbnail" src='+tracks[songIndex].artwork_url+'>'+tracks[songIndex].title+'</li></div>');
+        /*
+        SC.stream("/tracks/"+ songId).then(function(player){
+            player.play();
+            // inject html - will always do when a song is selected
+            $(".playlist").append('<div class="queued-song"><li class="track-playlist"><img class="thumbnail" src='+tracks[songIndex].artwork_url+'>'+tracks[songIndex].title+'</li></div>');
         }).catch(function() {
             console.log("failed streaming", arguments);
         });
+        */
         return false; // so we won't have the value put in the search box after selected
     },
     open: function () {
@@ -82,3 +96,10 @@ $("#search").autocomplete({
 
 });
 // end of autocomplete
+
+document.getElementById('play').addEventListener('click', function(){
+        if (currentPlayer) {
+            console.log("YAYYAYAYA", currentPlayer);
+            currentPlayer.pause();
+        }
+      });
