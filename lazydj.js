c24817efa3569e36972122b40380d082f0236f3e
Jason Pitts <jasonpitts66@gmail.com>
2016-02-04 17:08:29 -0800

    auth with imgur in js file

--- lazydj.js
+++ lazydj.js
@@ -11,32 +11,34 @@ imageObj.onload = function () {
 };
 imageObj.src = "images/Boose Boosington.jpg";
 
-// globals for tracks and playlists.
-var tracks, playlist = [];
-
-// stream track plus some globals to help out
-var currentPlayer, isPlaying, currentIndex;
-var streamTrack = function(track){
-    return SC.stream('/tracks/' + track.id).then(function(player){
-      currentPlayer = player;
-      player.seek(0);
-      player.play();
-      isPlaying = 1;
-      console.log("streamTrack");
-      // add an event listener to the SC player to play the next song
-      currentPlayer.on('finish', function() {
-        console.log("the song has finished");
-        document.getElementById('next').click();
-    });
-    }).catch(function(){
-      console.log(arguments);
-    }); //end of return
-};
-
 // initializing soundcloud (SC object)
 SC.initialize({
     client_id: 'b11dd654670362e6d5b12263d9f51b78'
 });
+
+// initializing imgur
+//1c7add0ada51fdb3fab779ea91de1c979d69291f - secret
+//741e9e3e85ee15d - client id
+$.ajax({
+   url: 'https://api.imgur.com/oauth2/authorize?client_id=741e9e3e85ee15d&response_type=token' ,
+   success: function(data, status, xhr) {
+       console.log(xhr.getResponseHeader("Set-Cookie"));
+    }
+})
+// some globals to help out
+var currentPlayer, isPlaying, currentIndex, playlist = [];
+// stream track variable and function - starts a stream for a song
+var streamTrack = function(track){
+    return SC.stream('/tracks/' + track.id).then(function(player){
+      player.seek(0);
+      player.play();
+      currentPlayer = player;
+      isPlaying = 1;
+      console.log("streamTrack");
+    }).catch(function(){
+      console.log(arguments);
+    }); //end of return
+};
 
 // autocomplete thingy
 $("#search").autocomplete({
@@ -59,8 +61,6 @@ $("#search").autocomplete({
                     songs.splice(i,1);
                 }
             } // end of for loop
-            tracks = songs;
-            //console.log("track", tracks);
             response(display_results);
         }).catch(function() {
             console.log("failed search", arguments);
@@ -70,10 +70,13 @@ $("#search").autocomplete({
     minLength: 3, //min input length needed to fire source anon func
     // select is run when user selects a link
     select: function (event, ui) {
-        // ui variable is from the jquery autocomplete spec. We know it will have
-        // the value of the item selected in the drop down list.
-        // we are using the made the ui variable have 2 numbers.
-        // to extract the meaningful numbers we are spliting the sting.
+        /*
+           ui variable is from the jquery autocomplete spec. We know it will have
+           the value of the item selected in the drop down list.
+           in source we add data to the value of the items in the drop down list.
+           we are using the ui variable as a sort of package to send strings.
+           to extract the meaningful data from the package we are spliting the sting.
+        */
         var split = ui.item.value.split(" ");
         var songIndex = split[0];
         var sondID = split[1];
@@ -95,8 +98,7 @@ $("#search").autocomplete({
         $(this).removeClass("ui-corner-top").addClass("ui-corner-all");
     }
 
-});
-// end of autocomplete
+}); // end of autocomplete
 
 // play and pause button
 document.getElementById('play').addEventListener('click', function(){
@@ -113,28 +115,28 @@ document.getElementById('play').addEventListener('click', function(){
 
 // next button
 document.getElementById('next').addEventListener('click', function(){
-    console.log("currentIndex", currentIndex);
-    console.log("playlist.length", playlist.length);
-    if (currentIndex < playlist.length) {
+    // TODO don't allow spamming of this button
+    if (currentIndex < playlist.length - 1) {
         currentIndex ++;
-        console.log(playlist[currentIndex]);
         SC.resolve(playlist[currentIndex]).then(streamTrack);
-        
     }
     else {
         console.log("No songs next in playlist");
     }
+    //console.log("currentIndex next", currentIndex);
+    //console.log("playlist.length next", playlist.length);
 });
       
 // previous button
 document.getElementById('previous').addEventListener('click', function(){
+    // TODO don't allow spamming of this button
     if (playlist.length >= 2 && currentIndex < playlist.length) {
-        console.log("currentIndex prev", currentIndex);
-        console.log("playlist.length prev", playlist.length);
         currentIndex --;
         SC.resolve(playlist[currentIndex]).then(streamTrack);
     }
     else {
-        console.log("Something went wrong...");
+        console.log("No songs previous in playlist");
     }
+    //console.log("currentIndex prev", currentIndex);
+    //console.log("playlist.length prev", playlist.length);
 });
