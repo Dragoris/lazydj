20c3f177af497f008777807ad5acac1ed53fa1b4
Dragoris <mtrower42@yahoo.com>
2016-05-26 17:39:17 -0700

    changed file paths
    online vs local

--- lazydj.js
+++ lazydj.js
@@ -117,7 +117,7 @@ function renderTitleBox(songTitle){
     console.log('titlebox', songTitle);
 }
 function togglePlayPause(toggle){
-    var file= "file:///C:/Users/Katelyn/Desktop/GitHub/lazydj/images/";
+    var file= "http://dragoris.github.io/lazydj/images/";
     if (document.getElementById('play-pause').src ==file +"pause.svg"){
         document.getElementById('play-pause').src = file +"play.svg";
     }else {
@@ -125,7 +125,7 @@ function togglePlayPause(toggle){
     }
 }
 function renderimage(change){
-    var file= "file:///C:/Users/Katelyn/Desktop/GitHub/lazydj/images/Backgrounds/";
+    var file= "http://dragoris.github.io/lazydj/images/Backgrounds/";
     var backgoundImages = [file+'1.jpg', file+'2.jpg', file+'3.jpg', file+'4.jpg', file+'5.jpg',
      file+'6.jpg', file+'7.jpg', file+'8.jpg', file+'9.jpg', file+'10.jpg', file+'11.jpg',
      file+'12.jpg', file+'13.jpg', file+'14.jpg', file+'15.jpg', file+'16.jpg', file+'17.jpg',
