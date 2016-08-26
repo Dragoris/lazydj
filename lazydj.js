619fc4f53e436abcb49cc4b16b31d2fe288db621
Dragoris <mtrower42@yahoo.com>
2016-08-25 17:27:40 -0700

    test
    test

--- lazydj.js
+++ lazydj.js
@@ -127,7 +127,7 @@ function togglePlayPause(toggle){
 
 }
 function renderimage(change){
-    var file= "images/backgrounds/";
+    var file= "images/Backgrounds/";
     var backgoundImages = [file+'1.jpg', file+'2.jpg', file+'3.jpg', file+'4.jpg', file+'5.jpg',
      file+'6.jpg', file+'7.jpg', file+'8.jpg', file+'9.jpg', file+'10.jpg', file+'11.jpg',
      file+'12.jpg', file+'13.jpg', file+'14.jpg', file+'15.jpg', file+'16.jpg', file+'17.jpg',
